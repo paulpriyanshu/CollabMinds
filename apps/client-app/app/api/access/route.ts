e3803f18f683e2a21e9494ad9c7333a6a4824ae6
Priyanshu Paul <priyanshupaul@Priyanshus-Laptop.local>
2024-07-22 21:14:46 +0530

    asda

--- apps/client-app/app/api/access/route.ts
+++ apps/client-app/app/api/access/route.ts
@@ -1,33 +1,34 @@
-// pages/api/auth/callback.ts
+// Import necessary types and functions
 import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
 import { getToken } from 'next-auth/jwt';
-import { NextApiRequest, NextApiResponse } from 'next';
-import { headers } from 'next/headers';
-// types.ts
+import { NextRequest } from 'next/server';
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+// Define the GET function
+export async function GET(req: NextRequest) {
   const session = await getServerSession();
   const token = await getToken({ req });
+  
   console.log("this is header", token?.access_token);
-  //console.log("JSON Web Token", JSON.stringify,null, 2);
-  console.log("this is second check",token?.access_token)
-  console.log("this is third check session",session)
+  console.log("this is second check", token?.access_token);
+  console.log("this is third check session", session);
+
   if (!session || !token?.access_token) {
     return NextResponse.json({ message: 'Not authenticated' });
   }
-//const API_KEY="AIzaSyB8-CIyt-2Yq_Q9NavKi3WQ-fOJskamfNs"
+
   try {
     const url = new URL('https://www.googleapis.com/youtube/v3/channels');
     url.searchParams.append('part', 'snippet,contentDetails,statistics');
     url.searchParams.append('mine', 'true');
-    url.searchParams.append('key',process.env.API_KEY||"");
+    url.searchParams.append('key', process.env.API_KEY || "");
 
     const response = await fetch(url.toString(), {
       headers: {
         Authorization: `Bearer ${token?.access_token}`,
       },
     });
+
     if (!response.ok) {
       const errorText = await response.text();
       console.error('Error fetching channel data:', response.status, response.statusText, errorText);
@@ -35,11 +36,9 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
     }
 
     const data = await response.json();
-    
-
     return NextResponse.json(data);
   } catch (error) {
     console.error('Error fetching channel data:', error);
     return NextResponse.json({ message: 'Error fetching channel data', error });
   }
-};
+}
